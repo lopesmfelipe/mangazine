7ce53bf0c516907602ee2976a95e79024b023705
Felipe Magalhaes Lopes <felipemlopes0009@gmail.com>
2023-10-16 10:27:39 -0400

    LIKE and BETWEEN query

--- uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,12 +1,46 @@
 package com.devsuperior.uri2602;
 
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
+import com.devsuperior.uri2602.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
-public class Uri2602Application {
+public class Uri2602Application implements CommandLineRunner {
+
+	@Autowired
+	private CustomerRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Uri2602Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		//SQL
+		List<CustomerMinProjection> list = repository.search1("rs");
+		List<CustomerMinDTO> result1 = list.stream().map(x -> new CustomerMinDTO(x)).collect(Collectors.toList());
+
+		System.out.println("\n*** Normal SQL result:");
+		for (CustomerMinDTO obj : result1) {
+			System.out.println(obj);
+		}
+
+		System.out.println("\n\n");
+
+		//JPQL
+		List<CustomerMinDTO> result2 = repository.search2("rs");
+
+		System.out.println("\n*** JPQL result:");
+		for (CustomerMinDTO obj : result2) {
+			System.out.println(obj);
+		}
 	}
 }

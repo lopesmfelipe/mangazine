48177dba6193e674cf20abf89227c317119dd2fc
lopesmfelipe <felipemlopes0009@gmail.com>
2024-04-27 10:39:28 -0400

    Error handling with db connection string, installing dependencies and dev dependencies,

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const morgan = require("morgan");
 
 const titleRouter = require("./routes/titleRoutes");
 const userRouter = require("./routes/userRoutes");
 
 const app = express();
-mongoose.set("strictQuery", false)
+mongoose.set("strictQuery", false);
 
 // MIDDLEWARES
 app.use(morgan("dev"));
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 // ROUTES
 app.use("/api/v1/tours", titleRouter);
--- server.js
+++ server.js
@@ -1,18 +1,27 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
 const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
-const CONNECTION = process.env.CONNECTION;
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 
-if (!CONNECTION) {
+if (!DB) {
   console.error("MongoDB connection string not provided");
   process.exit(1);
 }
 
 const start = async () => {
   try {
-    await mongoose.connect(CONNECTION);
-    app.listen(PORT, () => {
-      console.log("App listening on port " + PORT);
+    await mongoose.connect(DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
     });
   } catch (err) {
     console.log(err.message);
@@ -20,3 +29,11 @@ const start = async () => {
 };
 
 start();
+
+try {
+  app.listen(PORT, () => {
+    console.log("App running on PORT " + PORT);
+  });
+} catch (err) {
+  console.error(err.message);
+}

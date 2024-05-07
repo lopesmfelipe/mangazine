b4af7dd6376f977a3fa1306078904e34e5fac672
Felipe Lopes <felipemlopes0009@gmail.com>
2024-05-07 02:45:29 -0400

    Home page

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+//import Grid from "./components/Grid"
+//import Header from "./components/Header";
+import Home from "./pages/Home";
+
+function App() {
+
+  return (
+    <>
+      {/* <Grid />
+      <Header /> */}
+      <Home />
+    </>
+  );
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,27 @@
+import "../styles/general.css";
+import "../styles/home.css";
+
+const Home = () => {
+  return (
+    <section>
+      <header>
+        <div className="menu">
+          <button>Readlist</button>
+          <button>Profile</button>
+        </div>
+      </header>
+      <main>
+        <div className="text-container">
+          <h1 className="title">MANGABOOK</h1>
+          <h2>Mangas & Books All in One Place!</h2>
+          <h3>Search, rate, list and share your favorite mangas and books </h3>
+        </div>
+        <div className="searchbar-container">
+          <input type="text" placeholder="Search" className="searchbar" />
+        </div>
+      </main>
+    </section>
+  );
+};
+
+export default Home;

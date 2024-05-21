479d1bcf5d143dd900a5d507dd864100fc1af278
lopesmfelipe <felipemlopes0009@gmail.com>
2024-05-20 22:46:25 -0400

    Fetching titles with axios

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,24 +3,22 @@ import "./style.css";
 
 const Home = () => {
   return (
-    <section>
+    <>
       <header>
         <div className="menu">
           <button>Readlist</button>
           <button>Profile</button>
         </div>
       </header>
-      <main>
-        <div className="text-container">
-          <h1 className="title">MANGABOOK</h1>
-          <h2>Mangas & Books All in One Place!</h2>
-          <h3>Search, rate, list and share your favorite mangas and books </h3>
-        </div>
-        <div className="searchbar-container">
-          <input type="text" placeholder="Search" className="searchbar" />
-        </div>
-      </main>
-    </section>
+      <div className="text-container">
+        <h1 className="title">MANGABOOK</h1>
+        <h2>Mangas & Books All in One Place!</h2>
+        <h3>Search, rate, list and share your favorite mangas and books </h3>
+      </div>
+      <div className="searchbar-container">
+        <input type="text" placeholder="Search" className="searchbar" />
+      </div>
+    </>
   );
 };
 

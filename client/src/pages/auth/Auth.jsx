3fd20ecc023d9923731c04220be68513f70fc72a
Felipe Lopes <felipemlopes0009@gmail.com>
2024-06-22 10:31:10 -0400

    handle sign up

--- client/src/pages/auth/Auth.jsx
+++ client/src/pages/auth/Auth.jsx
@@ -10,10 +10,40 @@ import {
 import { Navigate } from "react-router-dom";
 
 export const Auth = () => {
+
+  // Function to handle signup success
+  const handleSignUpSuccess = async = (user) => {
+    const { userName, email, id } = user.details;
+
+    try {
+      // Send user data to backend
+      const response = await fetch('/api/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: userName,
+          email: email,
+          userId: id,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to create user')
+      }
+
+      console.log('Sucess! This is the response' + response);
+    } catch (err) {
+      console.error('Error creating user: ', err);
+    }
+
+  }
+
   return (
     <div className={classes["sign-in-container"]}>
       <SignedOut>
-        <SignUpButton mode="modal" />
+        <SignUpButton mode="modal" onSuccess={handleSignUpSuccess} />
         <SignInButton mode="modal" />
       </SignedOut>
 

802c722162429062a72c223ce08474275a8d09e4
Felipe Lopes <felipemlopes0009@gmail.com>
2024-07-23 04:22:03 -0400

    appError class

--- server/app.js
+++ server/app.js
@@ -25,4 +25,28 @@ app.use('/api/v1/user', userRouter);
 app.use('/api/v1/rating', ratingRouter);
 app.use('/api/v1/lists', listRouter);
 
+app.all('*', (req, res, next) => {
+  /* res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  }); */
+
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail'
+  err.statusCode = 404;
+
+  next(err );
+});
+
+// By specifying 4 parameters Express automatically knows that the function is an error handling middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
+
 module.exports = app;

61f3eb8b750b5e2bf85acd53980d03961180bc44
Felipe Lopes <felipemlopes0009@gmail.com>
2024-07-23 11:58:35 -0400

    Created error controller file and implemented the global error handler in the app.js

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const cors = require('cors');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+
 const titleRouter = require('./routes/titleRoutes');
 const userRouter = require('./routes/userRoutes');
 const ratingRouter = require('./routes/ratingRoutes');
@@ -26,27 +29,9 @@ app.use('/api/v1/rating', ratingRouter);
 app.use('/api/v1/lists', listRouter);
 
 app.all('*', (req, res, next) => {
-  /* res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
-  }); */
-
-  const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  err.status = 'fail'
-  err.statusCode = 404;
-
-  next(err );
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
-// By specifying 4 parameters Express automatically knows that the function is an error handling middleware
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
---
+++ server/controllers/errorController.js
@@ -0,0 +1,10 @@
+// By specifying 4 parameters Express automatically knows that the function is an error handling middleware
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
